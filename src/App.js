ff1648934e13256b209d90e170d21dc6dd117fe3
niruhan <niruhanv@gmail.com>
2021-11-18 15:31:12 +0530

    Change backend URL2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 }
 
 function callApi() {
-    fetch('https://simple-node-server-niru.herokuapp.com/', { method: 'GET' })
+    fetch('https://simple-node-server-niru.herokuapp.com/detailes', { method: 'GET' })
         .then(data => data.json())
         .then(json => alert(JSON.stringify(json)))
 }
